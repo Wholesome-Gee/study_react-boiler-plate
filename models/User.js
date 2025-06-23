3d7eca72a9940dc2b33615d0880d2181690bbbfc
Wholesome-Gee <jiyong0419@naver.com>
2025-06-23 18:35:37 +0900

    bcrypt로 해싱하기 추가내용

--- models/User.js
+++ models/User.js
@@ -14,16 +14,20 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre("save", function (next) {
   let user = this;
-  bcrypt.genSalt(saltRound, function (err, salt) {
-    // getSalt는 saltRound숫자만큼 해싱하는 소금을 생성하고, salt에 담는다.
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      // 대상을 salt로 해싱하고, 그 값을 hash에 담는다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRound, function (err, salt) {
+      // getSalt는 saltRound숫자만큼 해싱하는 소금을 생성하고, salt에 담는다.
       if (err) return next(err);
-      user.password = hash;
-      next();
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // 대상을 salt로 해싱하고, 그 값을 hash에 담는다.
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
     });
-  });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema); // db에 users collection이 생성됨
