71a01c57d1273807a1055c0eb3ada9c281c91991
Wholesome-Gee <jiyong0419@naver.com>
2025-06-23 14:57:31 +0900

    req.body 사용하기 위한 설정 & /join post요청에 응답코드 작성
    1. req.body를 위해
    express.urlencoded,
    express.json() 작성
    
    2. 클라이언트에서 /join 으로 post요청시 응답코드 작성

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 3300;
-const dbUrl =
-  "mongodb+srv://wholesome-gee:wlfyd1564@freecluster.0ds7963.mongodb.net/?retryWrites=true&w=majority&appName=FreeCluster";
+const { User } = require("./models/User");
 
+// mongodb와 연결
+const dbUrl =
+  "mongodb+srv://wholesome-gee:wlfyd1564@freecluster.0ds7963.mongodb.net/boiler-plate?retryWrites=true&w=majority&appName=FreeCluster/boiler-plate";
 const mongoose = require("mongoose");
 mongoose
   .connect(dbUrl)
   .then((res) => console.log("✅ db 연결"))
   .catch((err) => console.log("❌ db 연결 실패: ", err));
+
+// use 전역요청
+app.use(express.urlencoded({ extended: true })); // req.body를 사용하기 위한 설정1
+app.use(express.json()); // req.body를 사용하기 위한 설정2
+
+// get 요청
 app.get("/", (req, res) => res.send("main 페이지"));
+
+// post 요청
+app.post("/join", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    return res.json({ success: true, message: `⭕ 가입이 완료되었습니다.` });
+  } catch (err) {
+    console.log("❌ 가입실패: ", err);
+    return res.json({ success: false, message: `❌ 가입실패: ${err}` });
+  }
+});
 
 app.listen(port, () => console.log(`✅ server on : http://localhost:${port}`));
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: { type: String, maxlength: 16 },
+  email: { type: String, trim: true, unique: true },
   pasword: { type: String, minlength: 4 },
   manager: { type: Boolean, default: false },
   image: String,
