3c5d8f2011b600345c37628d97368e6a6d25522e
Wholesome-Gee <jiyong0419@naver.com>
2025-06-24 18:22:32 +0900

    /login post 요청 작업 3 (최종코드)
    인프런
    노드리액트 기초강의 #12
    
    1. return res.cookie('cookieName', user.token) 부분을 빼먹음

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
-const port = 3300;
-const { User } = require("./models/User");
-const config = require("./config/key"); // 환경변수 설정
 const cookieParser = require("cookie-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key"); // 환경변수 설정
+const port = 3300;
+const app = express();
 
 // mongodb와 연결
 const dbUrl = config.mongoURI;
@@ -47,7 +48,7 @@ app.post("/login", async (req, res) => {
     const token = await user.createToken();
     user.token = token;
     await user.save();
-    return res.json({ loginSuccess: true, message: "로그인 성공" });
+    return res.cookie("cookieName", user.token).status(200).json({ loginSuccess: true, message: "로그인 성공" });
   } catch (err) {
     return res.json({ loginSuccess: false, message: `server Error ${err}` });
   }
